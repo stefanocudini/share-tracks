6f41a6852e4160e0db9eb1e0575f654548e56289
z4k <none@none>
2012-12-05 12:38:38 +0100

    rimossi file js

--- pub.devel.js
+++ pub.devel.js
@@ -1,10 +1,10 @@
 
 var minZoom = 6,	//se fa troppo poco zoom deve caricare troppi marker!!
-	centerStart = new L.LatLng(42.243594, 12.502849),	//Soratte
+	//centerStart = new L.LatLng(42.243594, 12.502849),	//Soratte
 	gooLayer = new L.Google(),
-	osmLayer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'),
-  	gpxLayer = new L.LayerGroup(),
-	map = new L.Map('map', {center: centerStart, attributionControl: false, zoomControl: true});
+	//osmLayer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'),
+  	//gpxLayer = new L.LayerGroup(),
+	map = new L.Map('map', {center: centerStart, attributionControl: false});
  
 map.addLayer(gooLayer);
 
