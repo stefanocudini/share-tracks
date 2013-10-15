cf7abfa16918a9e0ab33ed581fd7588d731f82eb
z4k <stefano.cudini@gmail.com>
2013-10-15 16:26:36 +0200

    added new layers

--- share-tracks.devel.js
+++ share-tracks.devel.js
@@ -2,9 +2,12 @@
 var map = new L.Map('map', {attributionControl: false});
 	//osmLayer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'),
   	//gpxLayer = new L.LayerGroup(),
-var track;
+	//var osmLayer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+var osmLayer = new L.TileLayer('http://localhost/maps/osm-tile-cacher/tmsfake.php?{z}/{x}/{y}.png'),
+	cycleLayer = new L.TileLayer('http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png');
+	gooSatLayer = new L.Google();
 
-map.addLayer(new L.Google());
+map.addLayer(gooSatLayer);
 
 function zoomGpx(gpxline) {
 	map.fitBounds(gpxline.getBounds());	//zoom estensioni del gpx
@@ -32,10 +35,18 @@ gpxLayer
 
 map.addLayer(gpxLayer);
 
+map.addControl(new L.Control.Layers({
+	"Google Satellite": gooSatLayer,
+	"Openstreetmap": osmLayer,
+	"OSM Rilievo": cycleLayer
+	},{"GPX track": gpxLayer},{position:'topright'}) );
+
+
 var gpxzoom = L.DomUtil.get('gpxzoom');
 L.DomEvent
 	.disableClickPropagation(gpxzoom)
 	.addListener(gpxzoom, 'click', function() {
 		zoomGpx( gpxLayer );
 	},this);
+
 
